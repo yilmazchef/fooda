e54f11b8c1f7a737e83748f246c44393dba6ac5a
yilmazchef <yilmaz@mail.be>
2021-12-22 09:20:01 +0100

    maven build stage is complete.

--- frontend/store-pwa/src/main/java/be/fooda/frontend/store/data/generator/DataGenerator.java
+++ frontend/store-pwa/src/main/java/be/fooda/frontend/store/data/generator/DataGenerator.java
@@ -14,8 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import com.vaadin.exampledata.DataType;
-import com.vaadin.exampledata.ExampleDataGenerator;
 
 @SpringComponent
 public class DataGenerator {
